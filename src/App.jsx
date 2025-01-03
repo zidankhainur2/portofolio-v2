8e3224dc6e1eb6ac9f93fbfb1347c1688ab56c81
Ahmad Fauzidan <zidankhainur2@gmail.com>
2025-01-03 14:42:03 +0700

    disable tech components

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ const App = () => {
         </div>
         <About />
         {/* <Experience /> */}
-        <Tech />
+        {/* <Tech /> */}
         <div id="work">
           <Works />
         </div>
